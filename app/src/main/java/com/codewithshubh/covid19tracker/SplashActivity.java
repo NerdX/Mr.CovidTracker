9107d78e83adfa3401daba4cdbc2766c71c7bf60
NerdX <nerdx01salekar@gmail.com>
2020-11-11 14:16:41 +0530

    initial commit

---
+++ app/src/main/java/com/codewithshubh/covid19tracker/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.codewithshubh.covid19tracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+        Timer timer = new Timer ();
+
+        timer.schedule(new TimerTask () {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent (getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+    }
+
+}
