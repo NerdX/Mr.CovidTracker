526ef39e8a68c1a42739159564680b8aec6a3b11
NerdX <nerdx01salekar@gmail.com>
2020-11-11 14:59:53 +0530

    initial commit

--- app/src/test/java/com/example/mrcovidtracker/ExampleUnitTest.java
+++ app/src/test/java/com/example/mrcovidtracker/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.codewithshubh.covid19tracker;
+package com.example.mrcovidtracker;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
